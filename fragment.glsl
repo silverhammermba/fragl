0a5fe6405ed2535795762abce5b825f7ecf33aa1
Max <silverhammermba@gmail.com>
2013-07-10 19:34:13 -0400

    Change shaders to (try to) draw mandelbrot set

--- fragment.glsl
+++ fragment.glsl
@@ -4,7 +4,32 @@ in vec2 Position;
 
 out vec4 outColor;
 
+const int steps = 32;
+const float grade = 1.0 / steps;
+
+vec2 complex_square(vec2 v)
+{
+	return vec2(v.x * v.x - v.y * v.y, v.x * v.y * 2);
+}
+
+float abs_squared(vec2 v)
+{
+	return v.x * v.x + v.y * v.y;
+}
+
 void main()
 {
-	outColor = vec4(Position, 1.0, 1.0);
+	outColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+	vec2 z = Position;
+	for (int i = 0; i < steps; i++)
+	{
+		if (abs_squared(z) > 4.0)
+		{
+			outColor = vec4(grade * i, grade * i, grade * i, 1.0);
+			break;
+		}
+
+		z = complex_square(z) + z;
+	}
 }
--- vertex.glsl
+++ vertex.glsl
@@ -6,6 +6,6 @@ out vec2 Position;
 
 void main()
 {
-	Position = position;
+	Position = position * 2;
 	gl_Position = vec4(position, 0.0, 1.0);
 }

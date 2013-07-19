dad09b36587175f422d632c6f1b84ad326ff9e07
Max <silverhammermba@gmail.com>
2013-07-18 21:07:59 -0400

    randbow!

--- fragment.glsl
+++ fragment.glsl
@@ -16,7 +16,13 @@ float abs_squared(vec2 v)
 	return v.x * v.x + v.y * v.y;
 }
 
-mat3 colors = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0);
+vec3 colors[6] = vec3[](
+vec3(1.0, 0.0, 0.0),
+vec3(1.0, 0.5, 0.0),
+vec3(1.0, 1.0, 0.0),
+vec3(0.0, 1.0, 0.0),
+vec3(0.0, 0.0, 1.0),
+vec3(1.0, 0.0, 1.0));
 
 void main()
 {
@@ -28,7 +34,7 @@ void main()
 		if (abs_squared(z) > 4.0)
 		{
 			float grade = 1.0 - log(log(sqrt(abs_squared(z))) / log(2.0)) / log(2.0);
-			outColor = vec4(mix(colors[i % 3], colors[(i + 1) % 3], grade), 1.0);
+			outColor = vec4(mix(colors[i % 6], colors[(i + 1) % 6], grade), 1.0);
 			break;
 		}
 
